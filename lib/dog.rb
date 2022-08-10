bc77e755cbe66832c658817e7eec51412e8f002c
Nick Warren <nickwarren47@gmail.com>
2022-08-10 16:15:52 -0600

    commit tree 70e4f97f2881490ff499a1f508e3f3ede536f4a0

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
 
+    def name
+      @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+# => "Lassie"
+puts lassie.name
+
+# lassie.sit = "Dog sits"
+# lassie.give_treat = "Good dog!"
+
